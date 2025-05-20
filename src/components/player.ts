9389e4fa38d480d3f98d2c0a96a4dbc19561b2a7
EtherD3v <aymericduparc78@gmail.com>
2025-05-20 20:31:23 +0200

    feat(player) : add animation & restrict movement

--- src/components/player.ts
+++ src/components/player.ts
@@ -1,30 +1,21 @@
 // components/player.ts
 
 /** Player Management
- * D1 (18/05/2025) : feat & chore => enable movement / restrictions inside cube
+ * D1 (18/05/2025) : feat/chore : enable movement & restrictions inside cube
+ * D2 (20/05/2025) : feat(player) : add animation & restrict movement
+
 **/
 
 type ArrowKey = 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight';
 
 export function setupPlayer(player: HTMLDivElement): void {
 
-  let top: number = 140, left: number = 140;
+  const events: Record<ArrowKey, number[]> = {
 
-  const MIN_POS: number = 0, MAX_POS: number = 280;
-  
-  const events: Record<ArrowKey, number> = {
-
-    'ArrowUp': -10,
-    'ArrowDown': 10,
-    'ArrowLeft': -10,
-    'ArrowRight': 10,
-
-  }
-
-  const getDelta = (pos: number, eventKey: string): number => {
-
-    const offset: number = events[eventKey as ArrowKey] ?? 0;
-    return pos + offset >= MIN_POS && pos + offset <= MAX_POS ? offset : 0;
+    'ArrowUp': [0, -140],
+    'ArrowDown': [0, 140],
+    'ArrowLeft': [-140, 0],
+    'ArrowRight': [140, 0],
 
   }
 
@@ -32,12 +23,15 @@ export function setupPlayer(player: HTMLDivElement): void {
 
     const eventKey = event.key;
 
-    top += (eventKey === 'ArrowUp' || eventKey === 'ArrowDown') ? getDelta(top, eventKey) : 0;
-    left += (eventKey === 'ArrowLeft' || eventKey === 'ArrowRight') ? getDelta(left, eventKey) : 0;
+    player.style.setProperty('--move-x', `${events[eventKey][0]}px`);
+    player.style.setProperty('--move-y', `${events[eventKey][1]}px`);
+    player.classList.add('animate');
 
-    player.style.top = `${top}px`;
-    player.style.left = `${left}px`;
+    player.addEventListener('animationend', () => {
 
+      setTimeout(() => player.classList.remove('animate'), 500);
+    
+    });
   });
 }
 
